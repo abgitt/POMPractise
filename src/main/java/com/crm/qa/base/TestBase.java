07d65c81400e7657809ac4b0400183f5ef9e7852
abgitt <abhilash.ballavolu@gmail.com>
2018-10-28 11:29:05 +0530

    Page object model

---
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,50 @@
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.crm.qa.util.TestUtil;
+
+public class TestBase 
+{
+	static WebDriver dr;
+	static Properties prop;
+	
+	public TestBase()
+	{
+		try
+		{
+			prop = new Properties();
+			FileInputStream fis = new FileInputStream("C:\\Users\\ABI\\eclipse-workspace\\FreeCRMTest\\src\\main\\java\\com"
+					+"\\crm\\qa\\config\\config.properties");
+			prop.load(fis);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void initialization()
+	{
+		if(prop.getProperty("browser").equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			dr=new ChromeDriver();	
+		}
+		else if(prop.getProperty("browser").equals("FF"))
+		{
+			System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+			dr=new ChromeDriver();	
+		}
+		
+		dr.manage().deleteAllCookies();
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		dr.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIME_OUT, TimeUnit.SECONDS);
+	}
+	
+}
---
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.crm.qa.util;
+
+public class TestUtil {
+	
+	public static long PAGE_LOAD_TIME_OUT = 30;
+	public static long IMPLICIT_WAIT = 20;
+	
+	
+}
